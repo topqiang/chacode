56f39ff18e9562543fb808bb6bacbc7bc8472437
topqiang <topqiang@kanziqiang.top>
2017-03-15 11:26:47 +0800

    0314

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -18,6 +18,7 @@ class IndexController extends BaseController {
 
     public function city(){
         $city = M('City') -> select();
+        $this -> assign('ip',get_client_ip());
         $this -> assign('citylist',$city);
         $this -> display();
     }
