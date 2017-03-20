828773af6d93e090d61d47eea672c49242e94480
topqiang <topqiang@kanziqiang.top>
2017-03-20 09:54:31 +0800

    0320

--- Application/Admin/Controller/QcodeController.class.php
+++ Application/Admin/Controller/QcodeController.class.php
@@ -18,14 +18,30 @@ class QcodeController extends AdminBasicController{
      */
     public function qcodeList(){
 
-        if(!empty($_POST['name']))$where['name']=array('like','%'.$_POST['name'].'%');
-        if(!empty($_POST['creatcode']))$where['creatcode']=$_POST['creatcode'];
-        if(!empty($_POST['codenum']))$where['codenum']=$_POST['codenum'];
+        if(!empty($_POST['name'])){
+            $where['name']=array('like','%'.$_POST['name'].'%');
+            $map['name'] = $where['name'];
+        }
+        if(!empty($_POST['creatcode'])){
+            $where['creatcode']=$_POST['creatcode'];
+            $map['creatcode'] = $where['creatcode'];
+        }
+        if(!empty($_POST['codenum'])){
+            $where['codenum']=$_POST['codenum'];
+            $map['codenum'] = $where['codenum'];
+        }
+
 
         $where['status'] = array('neq' , '9');
+
         $count = $this -> qcode -> where( $where ) -> count();
         $page = new \Think\Page($count,15);
+        foreach($map as $key=>$val) {
+            $page->parameter[$key]   =   urlencode($val);
+        }
         $res=$this -> qcode -> where($where) -> limit($page->firstRow,$page->listRows) -> select();
+
+
         $this->assign('list',$res);
         $this->assign('page',$page->show());
         $this->display('qcodeList');
