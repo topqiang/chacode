97e3f5331788aabb9c6ae1758c495524b0bda0c8
topqiang <topqiang@kanziqiang.top>
2017-03-27 15:12:45 +0800

    0327

--- Application/Home/Controller/BaseController.class.php
+++ Application/Home/Controller/BaseController.class.php
@@ -6,6 +6,8 @@ class BaseController extends Controller{
 		$user = session('usid');
 		$this -> appid = "wx83c3034ad39a3d24";
 		$this -> scret = "ea225bd96b57b93dc4712f66f9e018e9";
+		dump($_SERVER);
+		exit();
 		$redirect_uri = "http://chacode.txunda.com/index.php?s=/Index/index";
 		$isweixin = preg_match('/MicroMessenger/',$_SERVER['HTTP_USER_AGENT']);
 		$state = $_REQUEST['state'];
