192422ea11b11bc29970b6eb57fac9bc5025c0d4
topqiang <topqiang@kanziqiang.top>
2017-03-27 16:04:22 +0800

    0327

--- Application/Admin/Controller/UserController.class.php
+++ Application/Admin/Controller/UserController.class.php
@@ -12,7 +12,7 @@ class UserController extends Controller{
 		}
 		$count =  $this -> user -> where($where)->count();
 		$page = new \Think\Page($count,15);
-		$userlist = $this -> user -> where($where) -> limit($page->firstRow,$page->listRows) -> select();
+		$userlist = $this -> user -> where($where) -> order('c_time desc') -> limit($page->firstRow,$page->listRows) -> select();
 		$this -> assign('userlist',$userlist);
 		$this -> assign('page',$page->show());
 		$this -> display();
