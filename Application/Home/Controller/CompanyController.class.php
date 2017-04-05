3930ddbd53d9de109a6eeda3a05eb6909d84e05d
topqiang <topqiang@kanziqiang.top>
2017-04-05 11:12:08 +0800

    0405

--- Application/Home/Controller/CompanyController.class.php
+++ Application/Home/Controller/CompanyController.class.php
@@ -4,12 +4,24 @@ use Think\Controller;
 class CompanyController extends BaseController{
 	public function _initialize(){
 		parent::_initialize();
+		$this -> goods = D('Goods');
 	}
 	public function company(){
 		$id = $_GET['id'];
 		$ratem = M('Company');
 		$rates = $ratem -> where( array( 'id' => $id ) ) -> select();
-		
+		if ($rates) {
+			$where['id'] = array('in' , $rates[0]['paygoods']);
+			$res = $this -> goods -> where($where) -> select();
+			$str = "";
+			foreach ($res as $key => $value) {
+				if ($key != 0) {
+					$str .=",";
+				}
+				$str .= $value['name'];
+			}
+			$rates[0]['paygoods'] = $str;
+		}
 		$this -> assign("company",$rates[0]);
 		$this -> display();
 	}
