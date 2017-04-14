55d7964c11bb84c4eae5fb2032036496d7bde88d
topqiang <topqiang@kanziqiang.top>
2017-04-14 11:47:17 +0800

    0414

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -23,7 +23,7 @@ return  array(
     'DB_HOST'               => '127.0.0.1', // 服务器地址
     'DB_NAME'               => 'chacode',          // 数据库名
     'DB_USER'               => 'chacode',      // 用户名
-    'DB_PWD'                => '123456',          // 密码
+    'DB_PWD'                => 'lyh503452',          // 密码
     'DB_PORT'               => '3306',        // 端口
     'DB_PREFIX'             => 'code_',    // 数据库表前缀
     'DB_FIELDTYPE_CHECK'    => false,       // 是否进行字段类型检查
