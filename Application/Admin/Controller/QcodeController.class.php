c944e05f0d3f9147c0ce071f1ff92116cd531bcc
topqiang <topqiang@kanziqiang.top>
2017-06-02 17:22:54 +0800

    0602

--- Application/Admin/Controller/QcodeController.class.php
+++ Application/Admin/Controller/QcodeController.class.php
@@ -10,9 +10,61 @@ class QcodeController extends AdminBasicController{
 
     public $article = '';
     public function _initialize(){
-        $this->qcode = D('Qcg');
+        $this -> qcg = M('Qcg');
+        $this -> qcode = M('Qcode');
+        $this -> log = M('Log');
+        $this -> company = M('Company');
+
         header("Content-type: text/html; charset=utf-8");
     }
+
+    public function fa(){
+        $wxcode = $_POST['wxcode'];
+        $com = $this -> company -> where( "wxcode=$wxcode" ) -> find();
+        if (empty($com)) {
+            apiResponse("error","经销商不存在！");
+        }
+        $compid = $com['id'];
+        $start = strtoupper($_POST['start']);
+        $end = strtoupper($_POST['end']);
+        if (substr($start,0,4) == substr($end,0,4)) {
+            $whe['codenum'] = array(array('egt',$start),array('elt',$end),'and');
+            $whe['curcomid'] = 0;
+            $count = $this -> qcg -> where( $whe ) -> find();
+            if ($count) {
+                $where['codenum'] = array(array('egt',$start),array('elt',$end),'and');
+                $data['curcomid'] = $compid;
+                $istrue = $this -> qcode -> where( $where ) -> save( $data );
+                if ($istrue) {
+                    $num = intval(substr($end,4,6)) - intval(substr($start,4,6))+1;
+                    $logobj = array(
+                        'fromcid' => 0,
+                        'tocid' => $compid,
+                        'begin' => $start,
+                        'code' => substr($start,0,4),
+                        'end' => $end, 
+                        'status' => 0, 
+                        'num' => $num,
+                        'time' => time(), 
+                        'gname' => $count['name']
+                        );
+                    $islog = $this -> log -> add($logobj);
+                    if ($islog) {
+                        apiResponse("success","发货成功！");
+                    }
+                }else{
+                    apiResponse("error","发货失败！");
+                }
+            }else{
+                apiResponse("error","未找到符合条件的产品！");
+            }
+
+        }else{
+            apiResponse("error","初始编码不同！");
+        }
+    }
+
+
     /**
      * 菜单列表
      */
@@ -34,12 +86,12 @@ class QcodeController extends AdminBasicController{
 
         $where['status'] = array('neq' , '9');
 
-        $count = $this -> qcode -> where( $where ) -> order('visnum desc,id desc') -> count();
+        $count = $this -> qcg -> where( $where ) -> order('visnum desc,id desc') -> count();
         $page = new \Think\Page($count,15);
         foreach($map as $key=>$val) {
             $page->parameter[$key]   =   $val;
         }
-        $res=$this -> qcode -> where($where) -> order('id desc') -> limit($page->firstRow,$page->listRows) -> select();
+        $res=$this -> qcg -> where($where) -> order('id desc') -> limit($page->firstRow,$page->listRows) -> select();
 
 
         $this->assign('list',$res);
