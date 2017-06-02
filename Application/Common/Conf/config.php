543ac9563ac45ec3644d7f1611cc3f3a7c020ad3
topqiang <topqiang@kanziqiang.top>
2017-06-02 13:09:09 +0800

    0602

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -21,7 +21,7 @@ return  array(
     /* 数据库设置 */
     'DB_TYPE'               => 'mysqli',     // 数据库类型
     'DB_HOST'               => '127.0.0.1', // 服务器地址
-    'DB_NAME'               => 'chacode',          // 数据库名
+    'DB_NAME'               => 'chacode1',          // 数据库名
     'DB_USER'               => 'root',      // 用户名
     'DB_PWD'                => '123456',          // 密码
     'DB_PORT'               => '3306',        // 端口
